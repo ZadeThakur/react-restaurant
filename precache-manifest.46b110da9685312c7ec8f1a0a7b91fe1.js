afdc5fe96523879c9cdf203c254b106ec0043a9a
Eshu Thakur (Ujjwal Jadoun) <37092836+ZadeThakur@users.noreply.github.com>
2019-06-24 04:56:14 -0230

    Updates

---
+++ precache-manifest.46b110da9685312c7ec8f1a0a7b91fe1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5b01e201486b50896e130e5358afd6",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "b7ca9bf04cb119a7553f",
+    "url": "/react-restaurant/static/css/main.7b66031a.chunk.css"
+  },
+  {
+    "revision": "f6d0a8d98e0c0ad908ab",
+    "url": "/react-restaurant/static/js/2.e3b34014.chunk.js"
+  },
+  {
+    "revision": "b7ca9bf04cb119a7553f",
+    "url": "/react-restaurant/static/js/main.a23e711d.chunk.js"
+  },
+  {
+    "revision": "6b72e92db4061458148c",
+    "url": "/react-restaurant/static/js/runtime~main.2bc201a1.js"
+  },
+  {
+    "revision": "c292fa8091f5bf2554924a8d9d6add13",
+    "url": "/react-restaurant/static/media/image1.c292fa80.jpg"
+  },
+  {
+    "revision": "a32edd8541629e6e5872a4a5033ce310",
+    "url": "/react-restaurant/static/media/image2.a32edd85.jpg"
+  },
+  {
+    "revision": "afaa7316173df5491fbbb943e51bdddd",
+    "url": "/react-restaurant/static/media/image3.afaa7316.jpg"
+  },
+  {
+    "revision": "a714442106e5ba0afc3275278df1e254",
+    "url": "/react-restaurant/static/media/image4.a7144421.jpg"
+  }
+]);
